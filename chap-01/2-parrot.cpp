0e0dbcd5b9a3c0714cad1a4fa9a61ef763a3d7a9
ljjh2000 <jacqueline93140@gmail.com>
2023-02-01 23:29:50 +0100

    chap1-1 & chap1-2

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,32 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /*
+    int number = 0;
+    do
+    {
+        std::cin >> number;
+        std::cout << "Craow " << number << "!" << std::endl;
+    }
+    while (number != -1); */
+
+    /*
+    int number = 0;
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cin >> number;
+        std::cout << "Craow " << number << "!" << std::endl;
+    }*/
+
+    for (int i = 0; i < 10; i++)
+    {
+        string word;
+        cin >> word;
+        cout << "Craow " << word << "!" << endl;
+    }
+    return 0;
+}
