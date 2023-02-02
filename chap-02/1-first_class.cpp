7b559eac8930cdb223b4b5fe1db53894d7d3557f
ljjh2000 <jacqueline93140@gmail.com>
2023-02-02 11:10:04 +0100

    chap 3

--- chap-02/1-first_class.cpp
+++ chap-02/1-first_class.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+    void set_name(const string& name) { _name = name; }
+    void set_age(unsigned int age) { _age = age; }
+
+    const string& get_name() const { return _name; }
+    unsigned int  get_age() const { return _age; }
+
+private:
+    string       _name;
+    unsigned int _age = 0u;
+};
 
 int main()
 {
-    // Person p;
+    Person p;
 
-    // p.set_name("Batman");
-    // p.set_age(23);
+    p.set_name("Batman");
+    p.set_age(23);
 
-    // std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << endl;
 
     return 0;
 }
--- chap-02/3-clone.cpp
+++ chap-02/3-clone.cpp
@@ -9,6 +9,11 @@ public:
         , _surname { surname }
     {}
 
+    Person(const Person& other)
+        : _name { other._name }
+        , _surname { other._surname }
+    {}
+
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const { return _age; }
 
@@ -20,12 +25,22 @@ private:
     unsigned int _age = 0u;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Person& person)
+{
+    return stream << "Person named '" << person.get_full_name() << "' is " << person.get_age()
+                  << " years old.";
+}
+
 int main()
 {
     Person batman { "Bruce", "Wayne" };
     batman.wait(23);
 
-    std::cout << "Person named '" << batman.get_full_name() << "' is " << batman.get_age() << " years old."
+    Person copy = batman;
+
+    std::cout << batman << std::endl;
+
+    std::cout << "Person named '" << copy.get_full_name() << "' is " << copy.get_age() << " years old."
               << std::endl;
 
     return 0;
